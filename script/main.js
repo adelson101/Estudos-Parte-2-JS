883be5c951e0e158450b4f23a3b4d547a7833786
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-01-03 11:55:35 -0300

    update

--- script/main.js
+++ script/main.js
@@ -1,2 +1,13 @@
 const botao = document.querySelectorAll('button.tecla');
-for (let i in botao) { botao[i].onclick = () => document.querySelector(`#som_${botao[i].classList[1]}`).play() ; }
+for (let i in botao) {
+     botao[i].onclick = () => document.querySelector(`#som_${botao[i].classList[1]}`).play();
+     botao[i].onkeydown = (e) => {
+        if (e.code=='Enter' || e.code=='Space') {
+            botao[i].classList.add('ativa');
+        }
+    } 
+     botao[i].onkeyup = () => botao[i].classList.remove('ativa');
+    }
+
+
+    
