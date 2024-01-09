57915c80e604b5da89df36cbb403e2331e467e65
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-01-09 20:47:32 -0300

    update

--- cartas-personagens-edfeec1395c2708f8b1b7b7370b6226658b976f3/src/index.js
+++ cartas-personagens-edfeec1395c2708f8b1b7b7370b6226658b976f3/src/index.js
@@ -1,6 +1,9 @@
 import { Personagem } from "./components/modulos/personagem.js";
+import { PersonagemView } from "./components/personagem-view.js";
 
 const PersonagemPedrinho = new Personagem('Pedrinho',5,'Mago');
+const PersonagemJose = new Personagem('Jose',3,'Arqueiro');
 
-console.log(PersonagemPedrinho);
+const personagens = [PersonagemPedrinho,PersonagemJose]
 
+new PersonagemView(personagens).render()
