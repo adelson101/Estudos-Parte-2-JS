52c9f72c10018c68c7fe748b0d33ab29bb920d15
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-01-10 20:39:17 -0300

    update

---
+++ cartas-personagens-edfeec1395c2708f8b1b7b7370b6226658b976f3/src/components/modulos/guerreiro.js
@@ -0,0 +1,17 @@
+import { Personagem } from "./personagem.js";
+
+export class Guerreiro extends Personagem {
+    forca
+    static tipo = 'Guerreiro'
+    static descricao = 'Você será esmagado pela fúria do Guerreiro!'
+
+    constructor(nome,forca) {
+        super(nome);
+        this.forca = forca;
+    }
+
+    obterInsignia() {
+        if(super.level>=7 && this.furia>=5) return 'Guerreiro furioso';
+        return super.obterInsignia();
+    }
+}
