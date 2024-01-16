ba029855c394ba8a2a1c7284ba262868cd872939
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-01-16 20:56:50 -0300

    update

--- Estudo004/script.js
+++ Estudo004/script.js
@@ -16,6 +16,7 @@ async function BuscarEMostrarVideos() {
                     <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
                     <h3 class="titulo-video">${video.titulo}</h3>
                     <p class="titulo-canal">${video.descricao}<p>
+                    <p class="categoria" hidden>${video.categoria}</p>
                 </div>
                 </li>
                 `;
@@ -27,3 +28,27 @@ async function BuscarEMostrarVideos() {
 }
 
 BuscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector('.pesquisar__input');
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll('.videos__item');
+
+    if(barraDePesquisa.value != "") {
+        for (let video of videos) {
+            let titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+            
+            if(!titulo.includes(valorFiltro)) {
+                video.style.display = 'none';
+            }else {
+                video.style.display = 'block';
+            }
+        
+        }
+    } else {
+        video.style.display = 'block';
+    }
+}
