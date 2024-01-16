95e787d6eb9eeece648c45b93facef96a54c7e1e
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-01-16 18:49:56 -0300

    update

--- Estudo004/script.js
+++ Estudo004/script.js
@@ -1,22 +1,29 @@
 const ContainerVideos = document.querySelector('.videos__container')
 
-const api = fetch("http://localhost:3000/videos")
-.then( res => res.json())
-.then( (videos) => {
-    videos.forEach( video => {
-        ContainerVideos.innerHTML += `
-        <li class="videos__item">
-        <iframe src="${video.url}" title=" ${video.titulo}" frameborder="0" allowfullscreen></iframe>
-        <div class="descricao-video">
-            <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
-            <h3 class="titulo-video">${video.titulo}</h3>
-            <p class="titulo-canal">${video.descricao}<p>
-        </div>
-        </li>
-        `;
-    });
+async function BuscarEMostrarVideos() {
+    try {
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos =  await busca.json();
+            
+        videos.forEach( video => {
+            if(video.categoria == "") {
+                throw new Error('Video não possui categoria');
+            } else {
+                ContainerVideos.innerHTML += `
+                <li class="videos__item">
+                <iframe src="${video.url}" title=" ${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
+                    <h3 class="titulo-video">${video.titulo}</h3>
+                    <p class="titulo-canal">${video.descricao}<p>
+                </div>
+                </li>
+                `;
+            }
+            });
+    }catch(error) {
+        ContainerVideos.innerHTML = `<p>Erro ao carregar os videos: ${error}</p>`;
     }
-)
-.catch( (error) => {
-    ContainerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
-});
+}
+
+BuscarEMostrarVideos();
