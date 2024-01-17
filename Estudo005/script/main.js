e106d02eeda88d5fbbb48072afaf2efee334aa88
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-01-17 20:07:35 -0300

    update

--- Estudo005/script/main.js
+++ Estudo005/script/main.js
@@ -1,1 +1,22 @@
-https://guilhermeonrails.github.io/casadocodigo/livros.json
+async function GetVideos(){
+    const ElementoLivro = document.getElementById('livros');
+
+    const AcessarAPI = await fetch('https://guilhermeonrails.github.io/casadocodigo/livros.json');
+    const ListaDeVideos = await AcessarAPI.json(AcessarAPI);
+
+    ListaDeVideos.forEach((video)=>{
+        ElementoLivro.innerHTML += `
+        <div class="livro">
+            <div class="tags">
+                <span class="tag">${video.categoria}</span>
+            </div>
+            <img class="livro__imagem" src="${video.imagem}" alt="${video.alt}">
+            <h2 class="livro__titulo">${video.titulo}</h2>
+            <p class="livro__descricao">${video.autor}</p>
+            <p class="livro__preco">${ video.preco.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+            <div id='quantidade' hidden>${video.quantidade}</div>
+        </div>`;
+    });
+}
+
+GetVideos();
