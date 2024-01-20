ce84cdb0dbe81ae25a229586235388a7ba414cfb
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-01-20 00:48:13 -0300

    update

---
+++ Estudo006/script/main.js
@@ -0,0 +1,10 @@
+let ListaDeVideos = [];
+var EndPoint = 'http://localhost:3000/videos';
+
+async function AcessandoAPI () {
+    const AcessandoAPI = await fetch(EndPoint);
+    ListaDeVideos = await AcessandoAPI.json();
+    GerarUIVideos(ListaDeVideos);
+}
+
+AcessandoAPI();
---
+++ Estudo006/script/videos.js
@@ -0,0 +1,18 @@
+function GerarUIVideos(videos){
+    const BoxVideos = document.getElementById('Container__video');
+    BoxVideos.innerHTML = '';
+
+    videos.forEach( (video) => {
+        BoxVideos.innerHTML += `
+        <li class="video__item">
+            <iframe width="100%" height="72%" src="${video.url}" title="${video.titulo}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img src="${video.imagem}" alt="logo do canal alura">
+                    <h3 class="video__titulo">${video.titulo}</h3>
+                    <p>${video.descricao}</p>
+                </div>
+        </li>
+        `;
+    });
+
+}
