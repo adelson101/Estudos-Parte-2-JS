cd28f84e74af6c471b3b893bec94c5b7c237568f
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-01-22 22:11:04 -0300

    Update main.js

--- Estudo006/script/main.js
+++ Estudo006/script/main.js
@@ -6,7 +6,7 @@ var EndPoint = 'http://localhost:3000/videos';
 async function AcessandoAPI () {
     const AcessandoAPI = await fetch(EndPoint);
     ListaDeVideos = await AcessandoAPI.json();
-    GerarUIVideos(ListaDeVideos);
+    GerarUIVideos(ListaDeVideos); 
 }
 
 AcessandoAPI();
